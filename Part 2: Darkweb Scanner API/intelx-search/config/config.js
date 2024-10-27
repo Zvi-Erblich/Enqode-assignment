36c91c5e58b2423092f0fafabb468430cd29608d
Zvi-Erblich <zvierb@gmail.com>
2024-10-28 00:41:33 +0200

    edit the readme file

--- Part 2: Darkweb Scanner API/intelx-search/config/config.js
+++ Part 2: Darkweb Scanner API/intelx-search/config/config.js
@@ -1,8 +1,8 @@
 
-console.log('API_KEY:', process.env.API_KEY);
+const API_KEY= process.env.API_KEY;
 
 const config = {
-    API_KEY: process.env.API_KEY,
+    API_KEY: API_KEY,
     API_URL: 'https://2.intelx.io/'
 };
 
